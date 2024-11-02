c5837f80191e788c2f52eaf9b2eb6cc63527fb04
LanlanMC <Langrowup@outlook.com>
2024-11-02 09:53:54 +0800

    1.1.1

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from ctypes import windll
 import wave
 import struct
-import pygame
 import os
+from ctypes import windll
 
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
+
+import pygame
 
 windll.shcore.SetProcessDpiAwareness(1)
 
@@ -13,13 +14,12 @@ SIZE = (720, 720)
 DOT_COLOR = (0, 255, 0)
 GRID_COLOR = (40, 40, 0)
 BG_COLOR = (16, 16, 16)
-FPS = 73.2
-frame_time = 1/FPS * 1000
+FPS = int(input("FPS> "))
 
 _ = tk.Tk()
-_.geometry("1x1+256+256")
-_.iconify()
-audio_file = askopenfilename(filetypes=(("波形文件", ".wav"),))
+_.geometry("1x1+512+512")
+audio_file = askopenfilename(filetypes=(("波形文件", ".wav"),), parent=_, title="选取音频")
+_.destroy()
 if not os.path.isfile(audio_file) and not audio_file.endswith(".wav"):
     exit()
 try:
@@ -28,7 +28,7 @@ except wave.Error as e:
     print(f"出错: {e}")
     exit()
 sample_rate = wro.getframerate()
-READ_LENGTH = int(wro.getframerate() / FPS)
+read_length = int(wro.getframerate() / FPS)
 
 pygame.init()
 
@@ -38,31 +38,27 @@ pygame.display.set_caption("Oscilloscope", "示波器")
 clock = pygame.time.Clock()
 
 
-def get_grid(size) -> pygame.Surface:
-    grd = pygame.Surface(size)
-    grd = grd.convert_alpha()
-    grd.set_alpha(128)
-    grd.fill(BG_COLOR)
+grid = pygame.Surface(SIZE)
+grid.fill(BG_COLOR)
 
-    for x in range(10):
-        pygame.draw.line(grd, GRID_COLOR, (x * SIZE[0] / 10, 0), (x * SIZE[0] / 10, SIZE[0]))
+for horizontal in range(10):
+    pygame.draw.line(grid, GRID_COLOR, (horizontal * SIZE[0] / 10, 0), (horizontal * SIZE[0] / 10, SIZE[0]))
 
-    for y in range(10):
-        pygame.draw.line(grd, GRID_COLOR, (0, y * SIZE[1] / 10), (SIZE[0], y * SIZE[1] / 10))
+for vertical in range(10):
+    pygame.draw.line(grid, GRID_COLOR, (0, vertical * SIZE[1] / 10), (SIZE[0], vertical * SIZE[1] / 10))
 
-    pygame.draw.line(grd, GRID_COLOR, (SIZE[0] / 2, 0), (SIZE[0] / 2, SIZE[0]), 3)
-    pygame.draw.line(grd, GRID_COLOR, (0, SIZE[1] / 2), (SIZE[0], SIZE[1] / 2), 3)
+pygame.draw.line(grid, GRID_COLOR, (SIZE[0] / 2, 0), (SIZE[0] / 2, SIZE[0]), 3)
+pygame.draw.line(grid, GRID_COLOR, (0, SIZE[1] / 2), (SIZE[0], SIZE[1] / 2), 3)
 
-    for x in range(100):
-        pygame.draw.line(grd, GRID_COLOR, (x * SIZE[0] / 100, SIZE[1] / 2 - 3), (x * SIZE[0] / 100, SIZE[1] / 2 + 3))
+for horizontal in range(100):
+    pygame.draw.line(grid, GRID_COLOR, (horizontal * SIZE[0] / 100, SIZE[1] / 2 - 3),
+                     (horizontal * SIZE[0] / 100, SIZE[1] / 2 + 3))
 
-    for y in range(100):
-        pygame.draw.line(grd, GRID_COLOR, (SIZE[0] / 2 - 3, y * SIZE[1] / 100), (SIZE[0] / 2 + 3, y * SIZE[1] / 100))
+for vertical in range(100):
+    pygame.draw.line(grid, GRID_COLOR, (SIZE[0] / 2 - 3, vertical * SIZE[1] / 100),
+                     (SIZE[0] / 2 + 3, vertical * SIZE[1] / 100))
 
-    return grd
-
-
-grid = get_grid(SIZE)
+grid = pygame.surfarray.pixels2d(grid)
 
 pygame.mixer.music.load(audio_file)
 pygame.mixer.music.set_volume(0.5)
@@ -72,29 +68,31 @@ font = pygame.font.SysFont("Microsoft YaHei UI", 18)
 latency = 0.0
 
 
-while True:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+try:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
-    if pygame.mixer.music.get_pos() % 333 < frame_time:
         latency = pygame.mixer.music.get_pos() - wro.tell() * 1000 / sample_rate
-        if abs(latency) > 100:
+        fps = 1 / (1 / FPS - max(min(latency / 15000, 0.00075), -0.00075))
+        read_length = int(wro.getframerate() / FPS)
+        if abs(latency) > 64:  # latency correction
             wro.setpos(pygame.mixer.music.get_pos() * sample_rate // 1000)
-    frames = wro.readframes(READ_LENGTH)
+        frames = wro.readframes(read_length)
 
-    screen.fill(BG_COLOR)
-    screen.blit(grid, grid.get_rect())
+        pygame.surfarray.blit_array(screen, grid)
 
-    for i in range(0, READ_LENGTH, 1):
-        r = struct.unpack('hh', frames[i * 4:i * 4 + 4])
-        x = int(r[1] * SIZE[0] / 65536) + SIZE[0] // 2
-        y = int(-r[0] * SIZE[1] / 65536) + SIZE[1] // 2
-        screen.set_at((y, x), DOT_COLOR)
-    screen.blit(font.render(f"音像延迟: {latency:.1f}ms", True, (225, 225, 225)
-                            if abs(latency) < 75 else (255, 100, 100)), (0, 0))
+        for left, right in struct.iter_unpack("hh", frames):
+            x = int(right * SIZE[0] / 65536) + SIZE[0] // 2
+            y = int(-left * SIZE[1] / 65536) + SIZE[1] // 2
+            screen.set_at((y, x), DOT_COLOR)
 
-    pygame.display.flip()
+        screen.blit(font.render(f"音像延迟: {latency:.1f}ms", True, (225, 225, 225)
+                                if abs(latency) < 48 else (255, 80, 80)), (0, 0))
 
+        pygame.display.flip()
+        clock.tick(FPS)
+except wave.Error:
+    pass
